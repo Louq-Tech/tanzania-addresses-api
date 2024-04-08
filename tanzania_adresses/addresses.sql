81d45bec501eda9c688b78952c489e777537712f
Louq_Tech <144584573+Louq-Tech@users.noreply.github.com>
2024-04-08 14:22:23 +0300

    Adding queries for the database

---
+++ tanzania_adresses/addresses.sql
@@ -0,0 +1,64 @@
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (1, 'Arusha', 'ARS', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (2, 'DAR ES SALAAM', 'DAR', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (3, 'Dodoma', 'DDM', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (4, 'Geita', 'OTH', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (5, 'Iringa', 'IRN', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (6, 'Kagera', 'KGR', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (7, 'Katavi', 'OTH', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (8, 'Kigoma', 'KGM', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (9, 'Kilimanjaro', 'KLM', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (10, 'Lindi', 'LIN', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (11, 'Manyara', 'MNY', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (12, 'Mara', 'MAR', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (13, 'Mbeya', 'MBY', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (14, 'Morogoro', 'MOR', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (15, 'Mtwara', 'MTR', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (16, 'Mwanza', 'MWZ', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (17, 'Njombe', 'OTH', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (18, 'Pwani', 'OTH', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (19, 'Rukwa', 'RKA', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (20, 'Ruvuma', 'RVM', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (21, 'Shinyanga', 'SHY', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (22, 'Simiyu', 'OTH', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (23, 'Singida', 'SIN', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (24, 'Songwe', 'OTH', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (25, 'Tabora', 'TBR', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (26, 'Tanga', 'TAN', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (27, 'Unguja Kaskazini', 'UNG', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (28, 'Unguja Kusini', 'UNG', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (29, 'Pemba Kaskazini', 'PEM', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (30, 'Pemba Kusini', 'PEM', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (31, 'Mjini Magharibi', 'UNG', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO regions (id, name, code, deleted_at, created_at, updated_at)
+VALUES (32, 'Overseas', '003', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
